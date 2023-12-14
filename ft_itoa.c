df3965d8b1f0d61a49ff67ff360aa6dbe3f6ef86
Ayako Prokopczuk <ayprokop@c1s13d60.42wolfsburg.de>
2023-12-14 15:13:54 +0100

    modi

--- ft_itoa.c
+++ ft_itoa.c
@@ -6,48 +6,97 @@
 /*   By: ayprokop <ayprokop@student.42wolfsburg.de> +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/13 16:36:02 by ayprokop          #+#    #+#             */
-/*   Updated: 2023/12/14 15:06:04 by ayprokop         ###   ########.fr       */
+/*   Updated: 2023/12/14 15:13:24 by ayprokop         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-static size_t	get_digits(int n)
+static int	ft_nlen(long long num)
 {
-	size_t	i;
+	int	nlen;
 
-	i = 1;
-	while (n /= 10)
-		i++;
-	return (i);
+	nlen = 0;
+	if (num == 0)
+		return (1);
+	else if (num < 0)
+	{
+		num *= -1;
+		nlen++;
+	}
+	while (num > 0)
+	{
+		num /= 10;
+		nlen++;
+	}
+	return (nlen);
 }
 
 char	*ft_itoa(int n)
 {
-	char	*str_num;
-	size_t	digits;
+	char		*nbr;
 	long long	num;
+	int			len_nbr;
 
 	num = n;
-	digits = get_digits(n);
-	if (n < 0)
+	len_nbr = ft_nlen(num);
+	nbr = (char *)malloc(sizeof(*nbr) * (len_nbr + 1));
+	if (!nbr)
+		return (NULL);
+	nbr[len_nbr--] = '\0';
+	if (num == 0)
+	{
+		nbr[0] = '0';
+	}
+	else if (num < 0)
 	{
 		num *= -1;
-		digits++;
+		nbr[0] = '-';
 	}
-	str_num = (char *)malloc(sizeof(char) * (digits + 1));
-	if (!str_num)
-		return (NULL);
-	*(str_num + digits) = 0;
-	while (digits--)
+	while (num > 0)
 	{
-		*(str_num + digits) = num % 10;
-		num = num / 10;
+		nbr[len_nbr--] = '0' + (num % 10);
+		num /= 10;
 	}
-	if (n < 0)
-		*(str_num + 0) = '0';
-	return (str_num);
+	return (nbr);
 }
+
+// static size_t	get_digits(int n)
+// {
+// 	size_t	i;
+
+// 	i = 1;
+// 	while (n /= 10)
+// 		i++;
+// 	return (i);
+// }
+
+// char	*ft_itoa(int n)
+// {
+// 	char	*str_num;
+// 	size_t	digits;
+// 	long long	num;
+
+// 	num = n;
+// 	digits = get_digits(n);
+// 	if (n < 0)
+// 	{
+// 		num *= -1;
+// 		digits++;
+// 	}
+// 	str_num = (char *)malloc(sizeof(char) * (digits + 1));
+// 	if (!str_num)
+// 		return (NULL);
+// 	*(str_num + digits) = 0;
+// 	while (digits--)
+// 	{
+// 		*(str_num + digits) = num % 10;
+// 		num = num / 10;
+// 	}
+// 	if (n < 0)
+// 		*(str_num + 0) = '0';
+// 	return (str_num);
+// }
 
 // int	main(void)
 // {
